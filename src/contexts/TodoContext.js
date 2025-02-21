7755d65b1ed5d35d5c8edd17249d62193ffc1847
Ekta Vikas <ekta.apradh@softlabsgroup.com>
2025-02-21 15:11:48 +0530

    first commit

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -8,11 +8,11 @@ export const TodoContext = createContext({
             todo:"Todo msg",
             completed: false,
         }
-    ],
-    addTodo: (todo) => {},
-    updatedTodo:(id, todo) =>{},
-    deleteTodo: (id) => {},
-    toggleComplete: (id) => {}
+    ]
+    // addTodo: (todo) => {},
+    // updatedTodo:(id, todo) =>{},
+    // deleteTodo: (id) => {},
+    // toggleComplete: (id) => {}
 })
 
 
